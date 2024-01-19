445aac74f4510b93375700adfd5ffe7c3314cfec
Resamx <Resamxbox@gmail.com>
2024-01-19 20:17:05 +0300

    Serach app v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Data from './Data';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredData = Data.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Table'>
+        <h1>Search App</h1>
+        <form action="/action_page.php">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+        <br></br>
+        <table>
+          <tr>
+            <th>Number</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+          {filteredData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,7 @@
+const Data = [
+    { id: 1, name: 'Alex', age: 25 },
+    { id: 2, name: 'Steve', age: 30 },
+    { id: 3, name: 'Frank', age: 22 },
+];
+
+export default Data;
